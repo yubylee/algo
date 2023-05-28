d3f73457d4f6e5a11aca9220dffc9772a20f5a89
LeeGunhyoung <yuby1003@naver.com>
2023-05-28 20:13:45 +0900

    최대공약수 구하기

---
+++ app/src/main/java/app_6/Main.java
@@ -0,0 +1,30 @@
+package app_6;
+
+public class Main {
+  public static void main(String[] args) {
+
+    int num1, num2;
+    num1 = 12;
+    num2 = 18;
+
+    int small;
+    int big;
+
+    if (num1 > num2) {
+      big = num1;
+      small = num2;
+    } else {
+      big = num2;
+      small = num1;
+    }
+
+    int gcd = 1; //최대공약수
+
+    for(int i=1 ; i<=small ; i++) {
+      if(big % i == 0 && small % i == 0) {
+        gcd = i;
+      }
+    }
+    System.out.println(gcd);
+  }
+}
---
+++ app/src/main/java/app_6/Main3.java
@@ -0,0 +1,30 @@
+package app_6;
+
+public class Main3 {
+  public static void main(String[] args) {
+
+    int num1, num2;
+    num1 = 12;
+    num2 = 18;
+
+    int small;
+    int big;
+
+    if (num1 > num2) {
+      big = num1;
+      small = num2;
+    } else {
+      big = num2;
+      small = num1;
+    }
+
+    int gcd = 1;
+
+    for (int i = 1; i <= small; i++) {
+      if(big % i == 0 && small % i ==0) {
+        gcd = i;
+      }
+    }
+    System.out.println(gcd);
+  }
+}
