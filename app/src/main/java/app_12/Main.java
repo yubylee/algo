6e697efbdd794068a52c9830aaddb9d860023f28
LeeGunhyoung <yuby1003@naver.com>
2023-05-30 00:55:20 +0900

    숫자사각형 3

---
+++ app/src/main/java/app_12/Main.java
@@ -0,0 +1,23 @@
+package app_12;
+
+public class Main {
+  public static void main(String[] args) {
+
+    int n = 4;
+    int arr[][] = new int[n][n];
+    int num = 1;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j ++) {
+        arr[j][i] = num;
+        num++;
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j ++) {
+        System.out.printf("%3d", arr[i][j]);
+      }
+      System.out.println();
+    }
+  }
+}
